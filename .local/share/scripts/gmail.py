de1461272c69cc287dfd37512fa9d9a5262f535f
Aljoscha Frey <aljoscha.fry@gmail.com>
2021-03-11 10:41:39 +0100

    Add gmail.py needed by gmail.desktop

---
+++ .local/share/scripts/gmail.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# Author: Kraen Hansen (www.creen.dk)
+# Date: February 2011
+import sys
+import os
+import urlparse
+
+if __name__ == '__main__':
+	command = ""
+	if len(sys.argv) == 2 and sys.argv[1] != "":
+		argument = urlparse.urlparse(sys.argv[1])
+		if(argument.scheme == 'mailto'):
+			mailoptionsSplitted = argument.path.replace("?","&").split("&")
+			options = {}
+			if(len(mailoptionsSplitted) >= 1):
+				options['to'] = mailoptionsSplitted[0]
+			for option in mailoptionsSplitted:
+				temp = option.split("=")
+				if(len(temp) == 2):
+					options[temp[0]] = temp[1]
+			#print options
+			command += "google-chrome --app='https://mail.google.com/mail?view=cm&tf=0"
+			if 'to' in options.keys():
+				command += "&to="+options['to']
+			if 'cc' in options.keys():
+				command += "&cc="+options['cc']
+			if 'bcc' in options.keys():
+				command += "&bcc="+options['bcc']
+			if 'subject' in options.keys():
+				command += "&su="+options['subject']
+			if 'body' in options.keys():
+				command += "&body="+options['body']
+			command += "'";
+		else:
+			print "Unknown scheme:",argument.scheme
+	else:
+		command = "google-chrome 'https://mail.google.com/mail/#inbox'"
+	os.system(command);
