e56e663a52ead7f8cfdc8c758f1a659bd90f8e4b
Aljoscha Frey <aljoscha.fry@gmail.com>
2021-03-12 14:45:16 +0100

    install: Use function for dot shortcut; set http user

--- .config/install/remote-install.sh
+++ .config/install/remote-install.sh
@@ -8,7 +8,7 @@ pip3 install setuptools pyyaml argparse coloredlogs
 
 cd
 git clone --bare https://github.com/aljoschua/dotfiles .config/dotfiles
-dot="git --git-dir=$HOME/.config/dotfiles --work-tree=$HOME"
-$dot checkout @ -- .config/dotfiles
-$dot -c user.name=a -c user.email=a stash
+dot() { git --git-dir=$HOME/.config/dotfiles --work-tree=$HOME "$@" }
+dot checkout @ -- .config/dotfiles
+dot -c user.name=a -c user.email=a stash
 echo "Now run '.config/install/install.py MODULE' (see install.py [-l|--list] for modules)"
