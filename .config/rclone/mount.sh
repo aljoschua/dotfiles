e776edc50bcafbc7cbed0844d8361d3fdf72075e
Aljoscha Frey <aljoscha.fry@gmail.com>
2021-10-17 15:18:29 +0200

    sysd+rclone: Use a template for all rclone mount files

---
+++ .config/rclone/mount.sh
@@ -0,0 +1,23 @@
+# vim:ft=sh
+
+instance=$1
+mount_opts="--use-mmap --dir-cache-time 1000h --poll-interval 15s \
+    --vfs-cache-mode full --tpslimit 10 \
+    --buffer-size 16M --vfs-read-chunk-size 100M"
+
+cd ${0%/*}
+if [ -r $instance ]; then
+    source $instance
+fi
+
+start() {
+    exec rclone mount $instance: ~/$instance $mount_opts
+}
+
+stop() {
+    fusermount -u $template
+}
+
+reload() {
+    pkill -HUP -u $USER -fo "rclone mount $instance:"
+}
