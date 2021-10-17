b43dcc4e84c25ae51a8a45636b59551d98592d3d
Aljoscha Frey <aljoscha.fry@gmail.com>
2021-10-17 12:24:29 +0200

    scripts: Use find instead of **/*
    Somehow, the shell expands the latter differently in this script.
    I checked $ setopt but nothing suspicious.. Maybe sometime later I'll
    figure this out.

--- .local/lib/scripts/gitbu.sh
+++ .local/lib/scripts/gitbu.sh
@@ -7,7 +7,7 @@ for repo in $(< $storage/dirs.lst); do
         cd ~/$repo
         dir=$storage/${repo//\//%}
         mkdir -p $dir
-        rsync -rR $(git check-ignore **/*) $dir
+        rsync -rR $(git check-ignore $(find)) $dir
     )
 done
 
