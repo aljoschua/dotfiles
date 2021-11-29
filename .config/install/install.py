b2d2650e9eebe1825777531d3b3a5f43633224a2
Aljoscha Frey <aljoscha.fry@gmail.com>
2021-11-29 18:59:26 +0100

    install: Make install modules idempotent; Rm --skip
    This removes the need for skipping modules, which simplifies invocation.

--- .config/install/install.py
+++ .config/install/install.py
@@ -10,7 +10,6 @@ parser = argparse.ArgumentParser(description='Install stuff from yaml file.')
 parser.add_argument('module', default='default', nargs='?',
         help='The target module to install')
 parser.add_argument('--list', '-l', action='store_true')
-parser.add_argument('--skip', '-s', action='append', default=[], metavar='module')
 args = parser.parse_args()
 
 coloredlogs.install()
@@ -28,16 +27,9 @@ def execute_command(command):
     logging.info("}")
     if os.system(f"set -e\n{command}"):
         logging.error(f"Command failed")
-        hint = f"Continue with: {__file__} {args.module}"
-        for s in args.skip:
-            hint += f" -s {s}"
-        logging.info(hint)
         exit()
 
 def load_module(name):
-    if name in args.skip:
-        logging.info(f"Skipping {name}")
-        return
     module = conf[name]
     logging.info(f"Installing {name}...")
     if 'dep' in module:
@@ -49,10 +41,5 @@ def load_module(name):
     if 'cmd' in module:
         execute_command(module['cmd'])
     logging.info(f"Installed {name}")
-    args.skip.append(name)
 
 load_module(args.module)
-hint = f"Install other modules with: {__file__} module"
-for s in args.skip:
-    hint += f" -s {s}"
-logging.info(hint)
