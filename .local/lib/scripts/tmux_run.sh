a1faa69b6e0be403a7d80608d2a358e828d8e8c1
Aljoscha Frey <aljoscha.fry@gmail.com>
2022-02-03 09:49:27 +0100

    tmux: Use conditionals for scratch key-binding

--- .local/lib/scripts/tmux_run.sh
+++ .local/lib/scripts/tmux_run.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
 
-revert() {
-    tmux unbind-key -n Enter
-}
-trap revert HUP INT TERM
-
-tmux bind-key -n Enter source-file ~/.local/lib/scripts/enter.tmux
 st -c shellux -- tmux new-session -As scratch &
 tmux new-window -t scratch:
-wait
-
-revert
