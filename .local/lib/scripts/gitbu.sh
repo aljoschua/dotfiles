8f3fc6711a60ff7b14e939d77495e4e020c9bf44
Aljoscha Frey <aljoscha.fry@gmail.com>
2022-10-03 14:09:05 +0200

    scripts: Refactor gitbu.sh

--- .local/lib/scripts/gitbu.sh
+++ .local/lib/scripts/gitbu.sh
@@ -2,6 +2,18 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 set -eu
 
+gitbu() {
+    local git_dir
+    local files
+    git_dir=$(git rev-parse --git-dir)
+    files=$'\n'
+    # all ignored
+    files+=$(find -not -path $git_dir/\* | git check-ignore --stdin) || true
+    # untracked
+    files+=$'\n'$(git ls-files -o --exclude-standard)
+    printf '%s\n' "${files[@]}" | rsync -v --files-from=- . $1
+}
+
 storage=$HOME/.local/share/gitbu
 projects=$HOME/.local/src
 for repo in $(ls $projects); do 
@@ -9,6 +21,6 @@ for repo in $(ls $projects); do
         cd $projects/$repo
         dir=$storage/${repo//\//%}
         mkdir -p $dir
-        rsync -rR $(git check-ignore $(find)) $dir # maybe try using git clean -dxn
+        gitbu $dir
     )
 done
